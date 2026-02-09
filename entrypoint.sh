94b2611c09cfc3962d2a511380a78e45dbf8e66b
sitronix1523-phish <sitronix1523@gmail.com>
2026-02-09 16:55:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,1 @@
-#!/usr/bin/env bash
-set -e
 
-PORT="${PORT:-8080}"
-
-# 修改 phish_server.listen_url => 0.0.0.0:$PORT
-jq --arg p "$PORT" '.phish_server.listen_url = ("0.0.0.0:" + $p)' /opt/gophish/config.json > /opt/gophish/config.json.tmp
-mv /opt/gophish/config.json.tmp /opt/gophish/config.json
-
-echo "[*] Starting gophish with phish_server on 0.0.0.0:${PORT}"
-cd /opt/gophish
-./gophish
